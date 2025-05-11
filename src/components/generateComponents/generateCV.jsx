794d015b8608cd5e5668e51fff0613301ee19909
Unpetrified <kingsleyobiefunao1@gmail.com>
2025-05-11 23:12:12 +0100

    Generate CV component completed

--- src/components/generateComponents/generateCV.jsx
+++ src/components/generateComponents/generateCV.jsx
@@ -9,7 +9,7 @@ export default function Generate({editEventHandler, currentResume}) {
         <main>
             <section className="personal-info">
                 <h2>{currentResume["Personal"]["name"]}</h2>
-                <hr className="name-hr"/>
+                <div className="div-name-hr"></div>
                 <section className="contact">
                     <div><img src={phoneIcon} className='contact-icon'/><span>{currentResume["Personal"]["telephone"]}</span></div>
                     <div><img src={emailIcon} className='contact-icon'/><span>{currentResume["Personal"]["email"]}</span></div>
@@ -19,7 +19,7 @@ export default function Generate({editEventHandler, currentResume}) {
             </section>
             <section className="education">
                 <h3>Education</h3>
-                <hr className="edu-hr" />
+                <div className="div-hr"></div>
                 {currentResume["Education"].map(school => {
                     return (
                         <section key={school["key"]}>
@@ -34,7 +34,7 @@ export default function Generate({editEventHandler, currentResume}) {
             </section>
             <section className="work">
                 <h3>Work</h3>
-                <hr className="work-hr" />
+                <div className="div-hr"></div>
                 {currentResume["Work"].map(work => {
                     return (
                         <section key={work["key"]}>
@@ -56,6 +56,49 @@ export default function Generate({editEventHandler, currentResume}) {
                     )
                 })}
             </section>
+            {currentResume["Custom"].map(customField => {
+                if (customField["type"] === "list") return (
+                    <section className="custom-list work" key={customField["key"]}>
+                        <h3>{customField["name"]}</h3>
+                        <div className="div-hr"></div>
+                        <ul className="experiences">
+                            {customField["list"].map(list => {
+                                return (
+                                    <li key={list["key"]}>
+                                        {list["value"]}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </section>
+                )
+                else return (
+                    <section className="custom-dated work" key={customField["key"]}>
+                        <h3>{customField["name"]}</h3>
+                        <div className="div-hr"></div>
+                        {customField["dated"].map(dated => {
+                            return (
+                                <section key={dated["key"]}>
+                                    <div className="work-header">
+                                        <h4 className="company-name">{dated["name"]}</h4>
+                                        <div><h6 className="start">{dated["start"]}</h6> - <h6 className="end">{dated["end"]}</h6></div>
+                                    </div>
+                                    <p className="position">{dated["position"]}</p>
+                                    <ul className="experiences">
+                                        {dated["experience"].map(exp => {
+                                            return (
+                                                <li key={exp["key"]}>
+                                                    {exp["value"]}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </section>
+                            )
+                    })}
+                    </section>
+                )
+            })}
             <button className='edit-btn' onClick={editEventHandler}>Edit</button>
         </main>
     )
