5faf33d2addfc6d7c2c5c9e4330f2b7a70ddeef9
Unpetrified <kingsleyobiefunao1@gmail.com>
2025-05-13 13:40:52 +0100

    Download button added

--- src/components/generateComponents/generateCV.jsx
+++ src/components/generateComponents/generateCV.jsx
@@ -8,7 +8,8 @@ import Buttons from './save';
 export default function Generate({editEventHandler, currentResume}) {
     return (
         <main>
-            <section className="personal-info">
+            <div className="resume">
+                <section className="personal-info">
                 <h2>{currentResume["Personal"]["name"]}</h2>
                 <div className="div-name-hr"></div>
                 <section className="contact">
@@ -17,89 +18,90 @@ export default function Generate({editEventHandler, currentResume}) {
                     <div><img src={mapIcon} className='contact-icon'/><span>{currentResume["Personal"]["address"]}</span></div>
                     <div><img src={jobIcon} className='contact-icon'/><span>{currentResume["Personal"]["role"]}</span></div>
                 </section>
-            </section>
-            <section className="education">
-                <h3>Education</h3>
-                <div className="div-hr"></div>
-                {currentResume["Education"].map(school => {
-                    return (
-                        <section key={school["key"]}>
-                            <div className="school-header">
-                                <h4 className="school-name">{school["name"]}</h4>
-                                <div><h6 className="start">{school["start"]}</h6> - <h6 className="end">{school["end"]}</h6></div>
-                            </div>
-                            <div className="degree">{school["degree"]}</div>
-                        </section>
-                    )
-                })}
-            </section>
-            <section className="work">
-                <h3>Work</h3>
-                <div className="div-hr"></div>
-                {currentResume["Work"].map(work => {
-                    return (
-                        <section key={work["key"]}>
-                            <div className="work-header">
-                                <h4 className="company-name">{work["name"]}</h4>
-                                <div><h6 className="start">{work["start"]}</h6> - <h6 className="end">{work["end"]}</h6></div>
-                            </div>
-                            <p className="position">{work["position"]}</p>
+                </section>
+                <section className="education">
+                    <h3>Education</h3>
+                    <div className="div-hr"></div>
+                    {currentResume["Education"].map(school => {
+                        return (
+                            <section key={school["key"]}>
+                                <div className="school-header">
+                                    <h4 className="school-name">{school["name"]}</h4>
+                                    <div><h6 className="start">{school["start"]}</h6> - <h6 className="end">{school["end"]}</h6></div>
+                                </div>
+                                <div className="degree">{school["degree"]}</div>
+                            </section>
+                        )
+                    })}
+                </section>
+                <section className="work">
+                    <h3>Work</h3>
+                    <div className="div-hr"></div>
+                    {currentResume["Work"].map(work => {
+                        return (
+                            <section key={work["key"]}>
+                                <div className="work-header">
+                                    <h4 className="company-name">{work["name"]}</h4>
+                                    <div><h6 className="start">{work["start"]}</h6> - <h6 className="end">{work["end"]}</h6></div>
+                                </div>
+                                <p className="position">{work["position"]}</p>
+                                <ul className="experiences">
+                                    {work["experience"].map(exp => {
+                                        return (
+                                            <li key={exp["key"]}>
+                                                {exp["value"]}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </section>
+                        )
+                    })}
+                </section>
+                {currentResume["Custom"].map(customField => {
+                    if (customField["type"] === "list") return (
+                        <section className="custom-list work" key={customField["key"]}>
+                            <h3>{customField["name"]}</h3>
+                            <div className="div-hr"></div>
                             <ul className="experiences">
-                                {work["experience"].map(exp => {
+                                {customField["list"].map(list => {
                                     return (
-                                        <li key={exp["key"]}>
-                                            {exp["value"]}
+                                        <li key={list["key"]}>
+                                            {list["value"]}
                                         </li>
                                     )
                                 })}
                             </ul>
                         </section>
                     )
-                })}
-            </section>
-            {currentResume["Custom"].map(customField => {
-                if (customField["type"] === "list") return (
-                    <section className="custom-list work" key={customField["key"]}>
-                        <h3>{customField["name"]}</h3>
-                        <div className="div-hr"></div>
-                        <ul className="experiences">
-                            {customField["list"].map(list => {
+                    else return (
+                        <section className="custom-dated work" key={customField["key"]}>
+                            <h3>{customField["name"]}</h3>
+                            <div className="div-hr"></div>
+                            {customField["dated"].map(dated => {
                                 return (
-                                    <li key={list["key"]}>
-                                        {list["value"]}
-                                    </li>
+                                    <section key={dated["key"]}>
+                                        <div className="work-header">
+                                            <h4 className="company-name">{dated["name"]}</h4>
+                                            <div><h6 className="start">{dated["start"]}</h6> - <h6 className="end">{dated["end"]}</h6></div>
+                                        </div>
+                                        <p className="position">{dated["position"]}</p>
+                                        <ul className="experiences">
+                                            {dated["experience"].map(exp => {
+                                                return (
+                                                    <li key={exp["key"]}>
+                                                        {exp["value"]}
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </section>
                                 )
-                            })}
-                        </ul>
-                    </section>
-                )
-                else return (
-                    <section className="custom-dated work" key={customField["key"]}>
-                        <h3>{customField["name"]}</h3>
-                        <div className="div-hr"></div>
-                        {customField["dated"].map(dated => {
-                            return (
-                                <section key={dated["key"]}>
-                                    <div className="work-header">
-                                        <h4 className="company-name">{dated["name"]}</h4>
-                                        <div><h6 className="start">{dated["start"]}</h6> - <h6 className="end">{dated["end"]}</h6></div>
-                                    </div>
-                                    <p className="position">{dated["position"]}</p>
-                                    <ul className="experiences">
-                                        {dated["experience"].map(exp => {
-                                            return (
-                                                <li key={exp["key"]}>
-                                                    {exp["value"]}
-                                                </li>
-                                            )
-                                        })}
-                                    </ul>
-                                </section>
-                            )
-                    })}
-                    </section>
-                )
-            })}
+                        })}
+                        </section>
+                    )
+                })}
+            </div>
             <Buttons/>
             <button className='edit-btn' onClick={editEventHandler}>Edit</button>
         </main>
--- src/components/generateComponents/save.jsx
+++ src/components/generateComponents/save.jsx
@@ -16,6 +16,48 @@ function DownloadBtn () {
     return <button onClick={download}>Download CV</button>
 }
 
-function download () {
+async function download () {
+    const element = document.querySelector(".resume"),
+        images = element.querySelectorAll("img");
+    
+    for (let img of images) {
+        let urlBase64 = await toDataUrl(img.src);
+        img.setAttribute("src", urlBase64);
+    }
 
+    import("html2pdf.js").then((html2pdf) => {
+    html2pdf.default()
+      .set({
+        margin: 0.5,
+        filename: "resume.pdf",
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+      })
+      .from(element)
+      .save();
+  });
 }
+
+async function toDataUrl (url) {
+    //Convert to base64
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+        var reader = new FileReader();
+        reader.onloadend = function () {
+            resolve(reader.result);
+        };
+        reader.readAsDataURL(xhr.response);
+        };
+        xhr.onerror = () => {
+        reject({
+            status: this.status,
+            statusText: xhr.statusText,
+        });
+        };
+        xhr.open("GET", url);
+        xhr.responseType = "blob";
+        xhr.send();
+    });
+};
